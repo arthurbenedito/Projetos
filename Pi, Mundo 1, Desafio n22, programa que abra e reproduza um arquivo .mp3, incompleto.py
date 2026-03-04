c18fccb545ed1aa62d3ceca7c0311440250c7123
arthurbenedito <arthurbendito1911@gmail.com>
2026-03-04 12:04:05 -0300

    Create Pi, Mundo 1, Desafio n22, programa que abra e reproduza um arquivo .mp3, incompleto.py

---
+++ Pi, Mundo 1, Desafio n22, programa que abra e reproduza um arquivo .mp3, incompleto.py
@@ -0,0 +1,7 @@
+import pygame
+pygame.init()
+pygame.mixer.music.load('The Cranberries - Linger.mp3')
+pygame.mixer.music.play()
+input()
+pygame.event.wait()
+#agora só funciona com input sei nem pq

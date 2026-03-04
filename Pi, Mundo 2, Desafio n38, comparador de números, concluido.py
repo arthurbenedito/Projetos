a121010d2ff36ba53f141d480dabfdfa07cbfd9c
arthurbenedito <arthurbendito1911@gmail.com>
2026-03-04 12:17:54 -0300

    Create Pi, Mundo 2, Desafio n38, comparador de números, concluido.py

---
+++ Pi, Mundo 2, Desafio n38, comparador de números, concluido.py
@@ -0,0 +1,11 @@
+v1=float(input('Informe o primeiro valor: '))
+v2=float(input('Informe o segundo valor: '))
+if v1>v2:
+    print('O primeiro valor é maior que o segundo valor')
+elif v1<v2:
+    print('O segundo valor é maior que o primeiro valor')
+else:
+    print('Os dois valores são iguais')
+print('='*15)
+print('FIM DO PROGRAMA')
+print('='*15)

54d149ec41cb0c4429ac6a0e96919ea31edb6e35
arthurbenedito <arthurbendito1911@gmail.com>
2026-03-04 12:05:48 -0300

    Create Pi, Mundo 1, Desafio n 24, analisador de números, concluído.py

---
+++ Pi, Mundo 1, Desafio n 24, analisador de números, concluído.py
@@ -0,0 +1,7 @@
+N=str(input('Informe um número: '))
+print('Analisando...')
+u=N[3:4]
+d=N[2:3]
+c=N[1:2]
+m=N[:1]
+print('O algarismo da unidade é {}\nO algarismo da dezena é {}\nO algarismo da centena é\nO alagarismo de milhar é {}'.format(u, d ,c, m))

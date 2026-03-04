4a49227cdf7e68c5d3e4ba8790cfc496e146edf0
arthurbenedito <arthurbendito1911@gmail.com>
2026-03-04 12:10:28 -0300

    Create Pi, Mundo 1, Desafio n28, indicando o primeiro e o último nome, concluído.py

---
+++ Pi, Mundo 1, Desafio n28, indicando o primeiro e o último nome, concluído.py
@@ -0,0 +1,8 @@
+N=str(input('Digite o seu nome: '))
+print('Muito prazer em te conhecer!')
+m0=N.strip()
+m1=(N.split())
+m2=(N.count(' '))
+I0=m1[0]
+I1=m1[m2]
+print('Seu primeiro nome é {}\nSeu último nome é {}'.format(I0,I1))
